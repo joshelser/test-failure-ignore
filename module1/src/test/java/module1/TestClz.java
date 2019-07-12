3b7f400b798645ee9e69ef1456b9c63e1b0957eb
Josh Elser <elserj@apache.org>
2019-07-12 11:10:14 -0400

    Initial commit

---
+++ module1/src/test/java/module1/TestClz.java
@@ -0,0 +1,12 @@
+package module1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestClz {
+
+  @Test
+  public void test() { 
+    Assert.fail("this fails");
+  }
+}
---
+++ module2/src/test/java/module2/Test1.java
@@ -0,0 +1,11 @@
+package module2;
+
+import org.junit.Test;
+
+public class Test1 {
+
+  @Test
+  public void test() {
+    System.out.println("Woo");
+  }
+}
